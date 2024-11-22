6dce101c04019cb36e794058508e7f2606873258
голубев дима <maska.l.v@mail.ru>
2024-11-22 18:53:10 +0500

    0

--- Tyuiu.GolubevDS.Sprint5.Task0.V19.Lib/DataService.cs
+++ Tyuiu.GolubevDS.Sprint5.Task0.V19.Lib/DataService.cs
@@ -9,10 +9,11 @@ public class DataService : ISprint5Task0V19
         string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
 
         double y = 6.425;
-        double result = (2*Math.Pow(x, 2) - 1)/ (Math.Sqrt(Math.Pow(x,2)-2));
-        y = Math.Round(result, 3);
-        File.WriteAllText(path, y.ToString());
+        double result = ((2 * Math.Pow(x, 2) - 1) / (Math.Sqrt(Math.Pow(x, 2) - 2)));
+
+        y = Math.Round(y, 3);
+        File.WriteAllText(path, Convert.ToString(y));
         return path;
-        
+
     }
 }
