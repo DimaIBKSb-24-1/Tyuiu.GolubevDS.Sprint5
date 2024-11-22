f612d1a360e7a21e2ce5cf26cd7cd5b0dfd6bc60
голубев дима <maska.l.v@mail.ru>
2024-11-22 18:57:11 +0500

    0

--- Tyuiu.GolubevDS.Sprint5.Task0.V19.Lib/DataService.cs
+++ Tyuiu.GolubevDS.Sprint5.Task0.V19.Lib/DataService.cs
@@ -6,7 +6,7 @@ public class DataService : ISprint5Task0V19
 {
     public string SaveToFileTextData(int x)
     {
-        string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+        string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
 
         double y = 6.425;
         double result = ((2 * Math.Pow(x, 2) - 1) / (Math.Sqrt(Math.Pow(x, 2) - 2)));
